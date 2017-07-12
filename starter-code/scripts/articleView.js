f6365f5110666da1ef3e5faa1b04edc1a6cc414c
Don Pham <wrathofdon@gmail.com>
2017-07-12 16:57:33 -0700

    added data-author and hide/show filters for author

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,6 +40,8 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      $('article').hide();
+      $('article[data-author]"' + $(this).val() + '"]').show();
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
